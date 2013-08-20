e57004d0c3cae8ca5db3ca3eb2bcc7b9bc1d2534
Thomas Tsou <tom@tsou.cc>
2013-08-20 18:55:33 -0400

    Transceiver52M: Generate RACH correlation sequence at initialization
    There is no temporal dependency on when the RACH sequence is generated,
    so there is no need for transceiver to create it in response to a
    command from GSM core. If we power on the transceiver, we will need
    the RACH sequence, so just allocate it during initialization.
    
    Signed-off-by: Thomas Tsou <tom@tsou.cc>

--- Transceiver52M/Transceiver.cpp
+++ Transceiver52M/Transceiver.cpp
@@ -501,7 +501,6 @@ void Transceiver::driveControl()
         // Prepare for thread start
         mPower = -20;
         mRadioInterface->start();
-        generateRACHSequence(mSPS);
 
         // Start radio interface threads.
         mFIFOServiceLoopThread->start((void * (*)(void*))FIFOServiceLoopAdapter,(void*) this);
