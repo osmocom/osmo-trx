RangeNetworks/openbts

Transceiver52M: Add clipping detection on RACH input
Alert user of overdriven RACH input indicated by a positive
threshold detector result. This indication serves as notification
that the receive RF gain level is too high for the configured
transceiver setup.

Signed-off-by: Tom Tsou <tom@tsou.cc>

Transceiver52M/Transceiver.cpp
@@ -332,7 +332,7 @@ SoftVector *Transceiver::pullRadioVector(GSM::Time &wTime,
 				      int &timingOffset)
 {
   bool needDFE = false;
-  bool success = false;
+  int success = 0;
   complex amplitude = 0.0;
   float TOA = 0.0, avg = 0.0;
 
@@ -404,10 +404,21 @@ SoftVector *Transceiver::pullRadioVector(GSM::Time &wTime,
   }
   else {
     // RACH burst
-    if (success = detectRACHBurst(*vectorBurst, 6.0, mSPSRx, &amplitude, &TOA))
+    success = detectRACHBurst(*vectorBurst, 6.0, mSPSRx, &amplitude, &TOA);
+    if (success > 0) {
       channelResponse[timeslot] = NULL;
-    else
+    } else if (success == 0) {
       mNoises.insert(avg);
+    } else {
+      if (success == -SIGERR_CLIP) {
+        LOG(ALERT) << "Clipping detected on RACH input";
+      } else {
+        LOG(ALERT) << "Unhandled RACH error";
+      }
+
+      delete rxBurst;
+      return NULL;
+    }
   }
 
   // demodulate burst

osmocom/osmo-trx

Transceiver52M/Transceiver.cpp
@@ -523,8 +523,16 @@ SoftVector *Transceiver::pullRadioVector(GSM::Time &wTime, int &RSSI,
   else
     success = detectRACH(state, *burst, amp, toa);
 
-  if (!success) {
+  if (success == 0) {
     state->mNoises.insert(avg);
+    delete radio_burst;
+    return NULL;
+  } else if (success < 0) {
+    if (success == -SIGERR_CLIP) {
+      LOG(ALERT) << "Clipping detected on RACH input";
+    } else {
+      LOG(ALERT) << "Unhandled RACH error";
+    }
     delete radio_burst;
     return NULL;
   }