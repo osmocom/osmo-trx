12b059ccc2aa1d083a03d6bd85b3205ea89d1bd1
Oliver Smith <osmith@sysmocom.de>
2018-12-12 16:03:38 +0100

    WIP: move comma_delimited_to_vector to Utils.cpp
    Change-Id: If3f0f682ca453c2b0a06175ec9626567932cfce6

---
+++ CommonLibs/Utils.cpp
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2018 sysmocom - s.f.m.c. GmbH
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+#include <vector>
+#include <string>
+#include <sstream>
+
+std::vector<std::string> comma_delimited_to_vector(char* opt)
+{
+	std::string str = std::string(opt);
+	std::vector<std::string> result;
+	std::stringstream ss(str);
+
+	while( ss.good() )
+	{
+	    std::string substr;
+	    getline(ss, substr, ',');
+	    result.push_back(substr);
+	}
+	return result;
+}
