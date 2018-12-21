6333b0fe80e4a2093d3bd4b0decc491384641327
Sylvain Munaut <tnt@246tNt.com>
2018-12-21 16:38:31 +0100

    tests: Rework the convolve_test
    Besides just general cleanup, the major changes are :
     - Always run the reference / base implementation and print
       those results. Theses should be consistent across platform
       and check the base implementation works
     - Run the optimized version and compare results with small
       error tolerance against the reference results.
    
    Change-Id: I4e203d2c4b778af77d630ed15d4cef6b0c0eb76d
    Signed-off-by: Sylvain Munaut <tnt@246tNt.com>

--- tests/Transceiver52M/convolve_test.c
+++ tests/Transceiver52M/convolve_test.c
@@ -1,142 +1,265 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <math.h>
+
 #include "convolve.h"
 
-#define TESTVEC_LEN 1000
-#define DO_INIT 1
+struct test_vec
+{
+	float *x;
+	float *h;
+	float *y_ref;
+	float *y_iut;
 
-float x_vect[TESTVEC_LEN];
-float y_vect[TESTVEC_LEN];
-float h_vect[TESTVEC_LEN];
-
-float *x;
-float *h;
-float *y;
+	int x_len;	/* Theses are in # of _floats_ ! */
+	int h_len;	/* Theses are in # of _floats_ ! */
+	int y_len;	/* Theses are in # of _floats_ ! */
+};
 
 /* Generate some random values for testing */
-void gen_floats(float *vect, int len)
+static void
+gen_floats(float *vect, int len)
 {
 	int i;
 	for (i = 0; i < len; i++) {
-		vect[i] = (float)rand()/(float)(RAND_MAX);
+		vect[i] = (float)rand()/(float)(RAND_MAX) - 0.5f;
 	}
 }
 
-/* Reset testvectors */
-static void reset_testvec(int seed)
-{
-	srand(seed);
-	memset(x_vect,0,sizeof(x_vect));
-	memset(y_vect,0,sizeof(y_vect));
-	memset(h_vect,0,sizeof(h_vect));
-
-	x=x_vect + TESTVEC_LEN/2;
-	y=y_vect + TESTVEC_LEN/2;
-	h=h_vect + TESTVEC_LEN/2;
-
-	gen_floats(x_vect,TESTVEC_LEN);
-	gen_floats(h_vect,TESTVEC_LEN);
-}
-
 /* Show float vector data cut and paste friendly */
-static void dump_floats(float *vect, int len, char *name)
+static void
+dump_floats(float *vect, int len, char *name)
 {
 	int i;
 
-	printf("float %s[] = {", name);
+	printf("float %s[] = {\n\t", name);
 	for(i = 0; i < len; i++) {
-
-		printf("%f",vect[i]);
-
-		if(i<len-1)
-			printf(",");
+		char *end;
+		if (i == len-1)
+			end = "\n";
+		else if ((i&3) == 3)
+			end = ",\n\t";
+		else
+			end = ", ";
+		printf("%14.7e%s", vect[i], end);
 	}
 	printf("}\n");
 }
 
+/* Compare float with tolerance of delta (absolute) and epsilon (relative) */
+static int
+compare_floats(float *v0, float *v1, int len, float delta, float epsilon)
+{
+	int i;
+
+	for (i=0; i<len; i++)
+	{
+		float a = v0[i];
+		float b = v1[i];
+
+		if (fabsf(a - b) < delta)
+			continue;
+
+		if (fabsf(1.0f - (a/b)) < epsilon)
+			continue;
+
+		return 1;
+	}
+
+	return 0;
+}
+
+
+/* Reset test vectors */
+static void
+test_vec_reset(struct test_vec *tv, int seed)
+{
+	srand(seed);
+
+	memset(tv->x, 0, tv->x_len * sizeof(float));
+	memset(tv->h, 0, tv->h_len * sizeof(float));
+	memset(tv->y_ref, 0, tv->y_len * sizeof(float));
+	memset(tv->y_iut, 0, tv->y_len * sizeof(float));
+
+	gen_floats(tv->x, tv->x_len);
+	gen_floats(tv->h, tv->h_len);
+}
+
+/* Allocate test vectors */
+static struct test_vec *
+test_vec_alloc(int x_len, int h_len)
+{
+	struct test_vec *tv;
+
+	tv = calloc(1, sizeof(struct test_vec));
+	if (!tv)
+		return NULL;
+
+	tv->x_len = x_len;
+	tv->h_len = h_len;
+	tv->y_len = x_len;	/* Results can never be longer than x */
+
+	tv->x = convolve_h_alloc(x_len);
+	tv->h = convolve_h_alloc(h_len);
+	tv->y_ref = convolve_h_alloc(tv->y_len);
+	tv->y_iut = convolve_h_alloc(tv->y_len);
+
+	test_vec_reset(tv, 0);
+
+	return tv;
+}
+
+/* Release test vectors */
+static void
+test_vec_release(struct test_vec *tv)
+{
+	if (!tv)
+		return;
+
+	free(tv->x);
+	free(tv->h);
+	free(tv->y_ref);
+	free(tv->y_iut);
+
+	free(tv);
+}
+
 /* Test complex convolution */
-static void test_convolve_complex(int h_len)
+static void
+test_convolve_complex(struct test_vec *tv, int h_len)
 {
 	int x_len;
-	int y_len;
-	int start;
-	int len;
+	int start, len;
+	int rv;
 
-	x_len=34;
-	y_len=26;
-	start=8;
-	len=26;
-	reset_testvec(0);
-	dump_floats(x,x_len,"x");
-	printf("\n");
-	dump_floats(h,h_len,"h");
-	printf("\n");
-	convolve_complex(x, x_len, h, h_len, y, y_len, start, len);
-	dump_floats(y,y_len,"y");
-	printf("\n");
+	test_vec_reset(tv, 0);
+
+	/* Compute params that fit within our test vectors */
+	x_len = tv->x_len / 2; /* float vs complex */
+	start = h_len - 1;
+	len   = x_len - start;
+
+	/* Run both 'base/ref' implementation and the potentially optimized one */
+	base_convolve_complex(
+		tv->x, x_len,
+		tv->h, h_len,
+		tv->y_ref, tv->y_len,
+		start, len
+	);
+
+	convolve_complex(
+		tv->x, x_len,
+		tv->h, h_len,
+		tv->y_iut, tv->y_len,
+		start, len
+	);
+
+	/* Print the 'ref' results. Those should be consistent across platforms */
+	dump_floats(tv->y_ref, 2 * len, "y_ref");
+
+	/* Compare to 'iut' ones with small tolerance for precision */
+	rv = compare_floats(tv->y_ref, tv->y_iut, len * 2, 1e-5f, 1e-5f);
+	printf("IUT: %s\n", rv ? "!!! FAIL !!!" : "PASS");
+
+	if (rv)
+		dump_floats(tv->y_iut, 2 * len, "y_iut");
 }
 
 /* Test real convolution */
-static void test_convolve_real(int h_len)
+static void
+test_convolve_real(struct test_vec *tv, int h_len)
 {
 	int x_len;
-	int y_len;
-	int start;
-	int len;
+	int start, len;
+	int rv;
 
-	x_len=34;
-	y_len=26;
-	start=8;
-	len=26;
-	reset_testvec(0);
-	dump_floats(x-30,2*x_len+30,"x");
-	printf("\n");
-	dump_floats(h,2*h_len,"h");
-	printf("\n");
-	convolve_real(x, x_len, h, h_len, y, y_len, start, len);
-	dump_floats(y,y_len,"y");
-	printf("\n");
+	test_vec_reset(tv, 0);
+
+	/* Compute params that fit within our test vectors */
+	x_len = tv->x_len / 2; /* float vs complex */
+	start = h_len - 1;
+	len   = x_len - start;
+
+	/* Run both 'base/ref' implementation and the potentially optimized one */
+	base_convolve_real(
+		tv->x, x_len,
+		tv->h, h_len,
+		tv->y_ref, tv->y_len,
+		start, len
+	);
+
+	convolve_real(
+		tv->x, x_len,
+		tv->h, h_len,
+		tv->y_iut, tv->y_len,
+		start, len
+	);
+
+	/* Print the 'ref' results. Those should be consistent across platforms */
+	dump_floats(tv->y_ref, 2 * len, "y_ref");
+
+	/* Compare to 'iut' ones with small tolerance for precision */
+	rv = compare_floats(tv->y_ref, tv->y_iut, len * 2, 1e-5f, 1e-5f);
+	printf("IUT: %s\n", rv ? "!!! FAIL !!!" : "PASS");
+
+	if (rv)
+		dump_floats(tv->y_iut, 2 * len, "y_iut");
 }
 
 int main(void)
 {
-#if DO_INIT == 1
+	struct test_vec *tv;
+	int i;
+
 	convolve_init();
-#endif
 
-	printf("==== TEST COMPLEX BASE IMPLEMENTATION ====\n");
-	test_convolve_complex(17);
+	/* Alloc test vectors */
+		/* All *2 is to account for the facts all vectors are actually
+		 * complex and need two floats */
+	tv = test_vec_alloc(100*2, 25*2);
 
-	printf("==== TEST COMPLEX SSE3 IMPLEMENTATION: (h_len%%4=0) ====\n");
-	test_convolve_complex(20);
+	/* Dump all input data to make sure we work off the same input data */
+	printf("==== TEST INPUT DATA ====\n");
+	dump_floats(tv->x, tv->x_len, "x");
+	dump_floats(tv->h, tv->h_len, "h");
+	printf("\n");
+	printf("\n");
 
-	printf("==== TEST COMPLEX SSE3 IMPLEMENTATION: (h_len%%8=0) ====\n");
-	test_convolve_complex(16);
+	/* Test complex */
+	printf("==== TEST COMPLEX ====\n");
+
+	for (i=4; i<=24; i+=4)
+	{
+		printf(" -- h_len = %d --\n", i);
+		test_convolve_complex(tv, i);
+		printf("\n");
+	}
+
+	printf(" -- h_len = %d --\n", 25);
+	test_convolve_complex(tv, 25);
 
 	printf("\n");
 	printf("\n");
 
-	printf("==== TEST REAL BASE IMPLEMENTATION ====\n");
-	test_convolve_real(17);
+	/* Test real */
+	printf("==== TEST REAL ====\n");
 
-	printf("==== TEST REAL SSE3 IMPLEMENTATION (hlen=4) ====\n");
-	test_convolve_real(4);
+	for (i=4; i<=24; i+=4)
+	{
+		printf(" -- h_len = %d --\n", i);
+		test_convolve_real(tv, i);
+		printf("\n");
+	}
 
-	printf("==== TEST REAL SSE3 IMPLEMENTATION (hlen=8) ====\n");
-	test_convolve_real(8);
+	printf(" -- h_len = %d --\n", 25);
+	test_convolve_real(tv, 25);
 
-	printf("==== TEST REAL SSE3 IMPLEMENTATION (hlen=12) ====\n");
-	test_convolve_real(12);
+	printf("\n");
+	printf("\n");
 
-	printf("==== TEST REAL SSE3 IMPLEMENTATION (hlen=16) ====\n");
-	test_convolve_real(16);
-
-	printf("==== TEST REAL SSE3 IMPLEMENTATION (hlen=20) ====\n");
-	test_convolve_real(20);
-
-	printf("==== TEST REAL SSE3 IMPLEMENTATION (h_len%%4=0) ====\n");
-	test_convolve_real(24);
+	/* All done ! */
+	test_vec_release(tv);
 
 	return 0;
 }
