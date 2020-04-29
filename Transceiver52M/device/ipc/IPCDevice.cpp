57ece5a46f0ec5b5f1518efe72414cea163bf732
Eric <ewild@sysmocom.de>
2020-04-29 17:26:17 +0200

    ipc: remove intermediate class used for debugging
    Change-Id: I2e8a93389cf2f6fc0b9a591f800c019427cf37e6

--- Transceiver52M/device/ipc/IPCDevice.cpp
+++ Transceiver52M/device/ipc/IPCDevice.cpp
@@ -1077,3 +1077,17 @@ bool IPCDevice::setRxFreq(double wFreq, size_t chan)
 
 	return ipc_sock_send(&sk_chan_state[chan], msg) < 0 ? false : true;
 }
+
+RadioDevice *RadioDevice::make(size_t tx_sps, size_t rx_sps, InterfaceType iface, size_t chans, double lo_offset,
+			       const std::vector<std::string> &tx_paths, const std::vector<std::string> &rx_paths)
+{
+	if (tx_sps != rx_sps) {
+		LOGC(DDEV, ERROR) << "IPC Requires tx_sps == rx_sps";
+		return NULL;
+	}
+	if (lo_offset != 0.0) {
+		LOGC(DDEV, ERROR) << "IPC doesn't support lo_offset";
+		return NULL;
+	}
+	return new IPCDevice(tx_sps, rx_sps, iface, chans, lo_offset, tx_paths, rx_paths);
+}
